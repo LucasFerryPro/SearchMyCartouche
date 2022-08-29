3dd613003df16775e84bb4fb271a2afc10e1f98a
superlulu90 <lucas.ferry.ecole@gmail.com>
2022-08-30 00:15:08 +0200

    alélouya

--- flask_app.py
+++ flask_app.py
@@ -40,15 +40,10 @@ def password():
     if request.method == 'POST':
         if request.form.get('password') == pwd:
             try:
-                find = []
                 filename = glob.glob('static/excel/*')[0][13:]
                 listeFinal = dataSorter(ExcelFile(os.path.join(app.config['UPLOAD_FOLDER'], filename)))[0]
                 keys = dataSorter(ExcelFile(os.path.join(app.config['UPLOAD_FOLDER'], filename)))[1]
-                if request.method == 'POST':
-                    for element in listeFinal:
-                        if request.form.get("texte") in element[keys[1]]:
-                            find.append(listeFinal.index(element))
-                return render_template('home.html', listeFinal=listeFinal, keys=keys, find=find)
+                return redirect(url_for('home', listeFinal=listeFinal, keys=keys, find=[]))
             except:
                 return redirect(url_for('upload_file'))
     return render_template('password.html')
